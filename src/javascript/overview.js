e1fa262decd9bba015931ed57a57923497346161
Unknown <jonorh@web.de>
2018-08-06 23:05:02 +0200

    Add form page, extend overview.js accordingly

--- src/javascript/overview.js
+++ src/javascript/overview.js
@@ -5,18 +5,22 @@ app.controller('content-controller', function($scope) {
     //
     $scope.showOverview = true;
     $scope.showSettings = false;
+    $scope.showForm = false;
 
     $scope.showSettingsContainer = function(){
         // modify the content
         $scope.showOverview = false;
         $scope.showSettings = true;
+        $scope.showForm = false;
 
         // modify the nav bar
         var overviewLink = document.getElementById("overview");
         var settingsLink = document.getElementById("settings");
+        var matchLink = document.getElementById("match");
 
         overviewLink.setAttribute("class", "");
         settingsLink.setAttribute("class", "active");
+        matchLink.setAttribute("class", "");
 
 
     }
@@ -25,12 +29,38 @@ app.controller('content-controller', function($scope) {
         // modify the content
         $scope.showOverview = true;
         $scope.showSettings = false;
+        $scope.showForm = false;
 
         // modify the nav bar
         var overviewLink = document.getElementById("overview");
         var settingsLink = document.getElementById("settings");
+        var matchLink = document.getElementById("match");
 
         overviewLink.setAttribute("class", "active");
         settingsLink.setAttribute("class", "");
+        matchLink.setAttribute("class", "");
     }
+
+    $scope.showAddMatch = function(){
+        // modify the content
+        $scope.showOverview = false;
+        $scope.showSettings = false;
+        $scope.showForm = true;
+
+        // modify the nav bar
+        var overviewLink = document.getElementById("overview");
+        var settingsLink = document.getElementById("settings");
+        var matchLink = document.getElementById("match");
+
+        overviewLink.setAttribute("class", "");
+        settingsLink.setAttribute("class", "");
+        matchLink.setAttribute("class", "active");
+    }
+});
+
+app.controller('form-controller', function($scope){
+    // define the heroes
+    $scope.heroes = [{id: 'Ana'}, {id: 'Bastion'}, {id: 'Brigitte'}, {id: 'D.VA'}, {id: 'Doomfist'}, {id: 'Genji'}, {id: 'Hanzo'}, {id: 'Junkrat'},
+    {id: 'Lúcio'}, {id: 'McCree'}, {id: 'Mei'}, {id: 'Mercy'}, {id: 'Moira'}, {id:"Orisa"}, {id:"Pharah"}, {id: "Reaper"}, {id:"Reinhardt"}, {id:"Zenyatta"},
+    {id: 'Roadhog'}, {id: 'Soldier: 76'}, {id: 'Sombra'}, {id: 'Symmetra'}, {id: 'Torbjörn'}, {id: 'Tracer'}, {id: 'Widowmaker'}, {id: 'Winston'}, {id: 'Zarya'}];
 });
